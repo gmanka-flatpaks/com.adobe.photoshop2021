b1b66d6f254bfebdc023e792477f0171a230c40f
gmanka <gmankab@gmail.com>
2025-01-08 00:18:27 +0400

    [u] use extra data

--- modules/launcher/photoshop.sh
+++ modules/launcher/photoshop.sh
@@ -4,11 +4,13 @@ export WINEDEBUG=-all
 export WINEDLLOVERRIDES="mscoree="
 export WINEPREFIX=$XDG_DATA_HOME/wine
 export WINE=wine64
-mkdir -p $WINEPREFIX
-wine64 wineboot
-ln -s "$WINEPREFIX/drive_c/windows/regedit.exe" "$WINEPREFIX/drive_c/windows/syswow64/regedit.exe"
+if [ ! -d "$WINEPREFIX" ]; then
+    mkdir -p $WINEPREFIX
+    wine64 wineboot
+    ln -s "$WINEPREFIX/drive_c/windows/regedit.exe" "$WINEPREFIX/drive_c/windows/syswow64/regedit.exe"
+fi
 if [ -n "$vd" ]; then
     winetricks vd="$vd"
 fi
-wine64 /app/photoshop2021/photoshop.exe
+wine64 /app/extra/photoshop2021/photoshop.exe
 
