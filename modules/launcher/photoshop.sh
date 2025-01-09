68048da411ac8793c5c57221b7389c620989921b
gmanka <gmankab@gmail.com>
2025-01-09 22:24:14 +0400

    [+] vkd3d

--- modules/launcher/photoshop.sh
+++ modules/launcher/photoshop.sh
@@ -4,17 +4,20 @@ export WINEDEBUG=-all
 export WINEDLLOVERRIDES="mscoree="
 export WINEPREFIX=$XDG_DATA_HOME/wine
 export WINE=wine64
+if [ -d /app/extra ]; then
+    export PHOTOSHOP_ROOT=/app/extra
+else
+    export PHOTOSHOP_ROOT=/app
+fi
 if [ ! -d "$WINEPREFIX" ]; then
     mkdir -p $WINEPREFIX
     wine64 wineboot
     ln -s "$WINEPREFIX/drive_c/windows/regedit.exe" "$WINEPREFIX/drive_c/windows/syswow64/regedit.exe"
+    wineserver -w
+    /app/vkd3d/setup_vkd3d_proton.sh install
 fi
 if [ -n "$vd" ]; then
     winetricks vd="$vd"
 fi
-if [ -d /app/extra ]; then
-    wine64 /app/extra/photoshop2021/photoshop.exe
-else
-    wine64 /app/photoshop2021/photoshop.exe
-fi
+wine64 $PHOTOSHOP_ROOT/photoshop2021/photoshop.exe
 
